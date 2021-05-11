e415520421cbc51cb5fcae2df471a69a9d8b6c1f
DanDanOvO <1021461238@qq.com>
2021-05-11 13:31:07 +0800

    v0.4

--- install_Debian.sh
+++ install_Debian.sh
@@ -1,4 +1,4 @@
-sudo apt -y update & sudo apt install -y openjdk-11 git htop screen & pip3 install requests & pip3 install subprocess.run
+apt install -y default-jdk git htop screen wget & chmod 777 main & mv main /usr/bin/run
 
 
 
