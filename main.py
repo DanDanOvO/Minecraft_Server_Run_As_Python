d6291357ea175e112a902bc15aadd9ef5441ba01
DanDanOvO <1021461238@qq.com>
2021-05-11 13:16:25 +0800

    v0.4

--- main.py
+++ main.py
@@ -11,11 +11,14 @@ def cli(u_input=0):
         print("===============功能列表==================")
         print("(1) 启动服务器           (2) 自动编译最新的spigot核心")
         print("(3) 修改启动参数         (4) 一键创建服务器")
-        #print("(5) 返回控制台           (6) 强制关闭服务器")
+        print("(5) Linux强制关闭服务器  (6)Windows强制关闭服务器")
         print("(0) 取消")
         print("===============使用方法==================")
         print("如果您首次运行且没有服务端,请先使用(2)编译核心,然后使用(4)创建服务器")
         print("如果您拥有服务端,请把服务端文件夹重命名为server并放到程序运行目录,并使用(1)启动服务器")
+        print("===============注意事项==================")
+        print("Linux命令行用户运行前请先使用screen -S mcserver创建一个窗口再使用此程序")
+        print("请谨慎使用功能 (5) (6) 有丢失玩家存档的风险")
         print(raw_tip)
         try:
             u_input = input("请输入命令编号：")
@@ -23,7 +26,7 @@ def cli(u_input=0):
         except:
             u_input = 0
 
-    nums = [1, 2, 3, 4, ]#5, 6]
+    nums = [1, 2, 3, 4, 5, 6]
     if not u_input in nums:
         print(raw_tip)
         print("已取消!")
@@ -46,11 +49,10 @@ def cli(u_input=0):
     elif u_input == 4:
         AutoRUN.autorun()
         RunServer.start_server()
-    #elif u_input == 5:
-    #    runshell.run_shell("screen -r mcserver")
-    #elif u_input == 6:
-    #    runshell.run_shell("screen -S mcserver -X quit")
-
+    elif u_input == 5:
+        runshell.run_shell("ps -ef | grep java | grep -v grep | awk '{print $2}' | xargs kill -9")
+    elif u_input == 6:
+        runshell.run_shell("taskkill /F /IM java.exe")
 
 
 cli()
